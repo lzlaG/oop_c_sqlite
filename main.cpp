03ab478c8d48a9c38693e37a3f67031d662741d2
lzlaG <p0chta1234prosto@yandex.ru>
2024-05-10 00:26:01 +0500

    get parametrs func

--- main.cpp
+++ main.cpp
@@ -13,7 +13,60 @@ int DBMutantContainerIterator::GetCount()
     result = sqlite3_step(stmt);
     int count = sqlite3_column_int(stmt, 0);
     Count = count;
+    sqlite3_finalize(stmt);
     return count;
+};
+
+
+string DBMutantContainerIterator::GetHandPower()
+{
+    sqlite3_stmt* stmt;
+    const char *sql = "SELECT StrengthOfHands FROM Mutants WHERE ID = ?;";
+    int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
+    sqlite3_bind_int(stmt, 1, CurrentId);
+    rc = sqlite3_step(stmt);
+    string HandPower = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+    sqlite3_finalize(stmt);
+    return HandPower;
+};
+
+
+string DBMutantContainerIterator::GetLegPower()
+{
+    sqlite3_stmt* stmt;
+    const char *sql = "SELECT StrengthOfLegs FROM Mutants WHERE ID = ?;";
+    int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
+    sqlite3_bind_int(stmt, 1, CurrentId);
+    rc = sqlite3_step(stmt);
+    string LegPower = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+    sqlite3_finalize(stmt);
+    return LegPower;
+};
+
+
+string DBMutantContainerIterator::GetAge()
+{
+    sqlite3_stmt* stmt;
+    const char *sql = "SELECT Age FROM Mutants WHERE ID = ?;";
+    int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
+    sqlite3_bind_int(stmt, 1, CurrentId);
+    rc = sqlite3_step(stmt);
+    string Age = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+    sqlite3_finalize(stmt);
+    return Age;
+};
+
+
+string DBMutantContainerIterator::GetType()
+{
+    sqlite3_stmt* stmt;
+    const char *sql = "SELECT MutantType FROM Mutants WHERE ID = ?;";
+    int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
+    sqlite3_bind_int(stmt, 1, CurrentId);
+    rc = sqlite3_step(stmt);
+    string TypeOfMutant = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+    sqlite3_finalize(stmt);
+    return TypeOfMutant;
 };
 
 
@@ -23,12 +76,13 @@ void DBMutantContainerIterator::First()
     const char *sql = "SELECT id FROM Mutants ORDER BY id ASC LIMIT 1;";
     int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
     rc = sqlite3_step(stmt);
-    int id = sqlite3_column_int(stmt, 0);
+    CurrentId = sqlite3_column_int(stmt, 0);
     sqlite3_finalize(stmt);
 };
-
 
 int main()
 {
     DBMutantContainerIterator it("mydb.db");
+    it.First();
+    cout << it.GetType() << "\n";
 }
--- main.h
+++ main.h
@@ -7,16 +7,20 @@ class DBMutantContainerIterator
 private:
     int CurrentId;
     sqlite3 * DB;
+    int Count;
 public:
     DBMutantContainerIterator(const string& DBName)
     {
         int db = sqlite3_open(DBName.c_str(), &DB);
     };
     void First();
-    void GetCount();
-    //void Next() {CurrentId++;};
-    //bool IsDone() {return 1;};
-    //ScumPointer GetCurrent() {return * ScumCell;};
+    int GetCount();
+    string GetHandPower();
+    string GetLegPower();
+    string GetType();
+    string GetAge();
+    void Next() {CurrentId++;};
+    bool IsDone(int i) {return i>Count;};
 };
 
 #endif FC_sqlH
