a3118454c31e6c16531bd285a9b5ccd1a8abe783
lzlaG <p0chta1234prosto@yandex.ru>
2024-05-10 09:43:29 +0500

    iterator, functions for conntainer, container

--- main.cpp
+++ main.cpp
@@ -5,32 +5,84 @@
 
 using namespace std;
 
-
-int DBMutantContainerIterator::GetCount()
+//-------------- classes -----------------
+class Gargoyle : public Scum
 {
-    sqlite3_stmt* stmt;
-    int result = sqlite3_prepare_v2(DB, "SELECT COUNT(*) FROM Mutants", -1, &stmt, 0);
-    result = sqlite3_step(stmt);
-    int count = sqlite3_column_int(stmt, 0);
-    Count = count;
-    sqlite3_finalize(stmt);
-    return count;
+    public:
+        MutantType GetType() const {return MutantType::Gargoyle;};
+        void Fly(int x) const {cout << "Горгулья пролетела " << x << " км" << endl;}
+        void Summon()   const { cout << " Призвали горгулью " << endl;};
+        void Kill() const { cout << " убили горгулью " << endl;};
+};
+
+class Wolfman : public Scum
+{
+    public:
+        MutantType GetType() const {return MutantType::Wolfman;};
+        void Run(int x) { cout << "Оборотень пробежал " << x << " км " << endl;}
+        void Summon() const { cout << " Призвали оборотня " << "\n";};
+        void Kill() const { cout << " убили оборотня " << "\n";};
+};
+
+class Vampire : public Scum
+{
+    public:
+        MutantType GetType() const {return MutantType::Vampire;}
+        void Blood(int x) {cout << "Вампир выпил " << x << " литров крови " << endl;}
+        void Summon() const { cout << " Призвали вампира " << "\n";}
+        void Kill() const { cout << " убили вампира" << "\n";}
+};
+
+//---------- function for add string in sql------------------
+string quotesql( const string& s ) {
+    return string("'") + s + string("'");
+};
+
+//------pirnts-------------
+string PrintMutantType(const MutantType type)
+{
+    switch(type)
+	{
+		case MutantType::Gargoyle: return  "ГОРГУЛЬЯ";
+		case MutantType::Vampire: return  "ВАМПИР";
+		case MutantType::Wolfman: return  "ОБОРОТЕНЬ";
+		default: return "неизвестный";
+	}
+};
+
+string PrintLegPower (const StregthOfLegs type)
+{
+    switch (type)
+    {
+        case StregthOfLegs::Low: return "Слабые ноги";
+        case StregthOfLegs::Medium: return "Средние ноги";
+        case StregthOfLegs::High: return "Сильные ноги";
+    }
+};
+
+string PrintHandPower (const StregthOfHands type)
+{
+    switch (type)
+    {
+        case StregthOfHands::Low: return "Слабые руки";
+        case StregthOfHands::Medium: return "Средние руки";
+        case StregthOfHands::High: return "Мощные руки";
+    }
+};
+
+string PrintAgeOfMutant(const Age type)
+{
+    switch(type)
+    {
+        case Age::Old: return "ПОЖИЛОЙ";
+        case Age::Young: return "МОЛОДОЙ";
+        case Age::Newborn: return "Новорожденный";
+    }
 };
 
 
-string DBMutantContainerIterator::GetHandPower()
-{
-    sqlite3_stmt* stmt;
-    const char *sql = "SELECT StrengthOfHands FROM Mutants WHERE ID = ?;";
-    int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
-    sqlite3_bind_int(stmt, 1, CurrentId);
-    rc = sqlite3_step(stmt);
-    string HandPower = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-    sqlite3_finalize(stmt);
-    return HandPower;
-};
 
-
+//--------------db iterator----------------------
 string DBMutantContainerIterator::GetLegPower()
 {
     sqlite3_stmt* stmt;
@@ -43,6 +95,18 @@ string DBMutantContainerIterator::GetLegPower()
     return LegPower;
 };
 
+//----------- db iterator functions----------------
+string DBMutantContainerIterator::GetHandPower()
+{
+    sqlite3_stmt* stmt;
+    const char *sql = "SELECT StrengthOfHands FROM Mutants WHERE ID = ?;";
+    int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
+    sqlite3_bind_int(stmt, 1, CurrentId);
+    rc = sqlite3_step(stmt);
+    string HandPower = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+    sqlite3_finalize(stmt);
+    return HandPower;
+};
 
 string DBMutantContainerIterator::GetAge()
 {
@@ -69,20 +133,91 @@ string DBMutantContainerIterator::GetType()
     return TypeOfMutant;
 };
 
+int DBMutantContainerIterator::GetCount()
+{
+    sqlite3_stmt* stmt;
+    int result = sqlite3_prepare_v2(DB, "SELECT COUNT(*) FROM Mutants", -1, &stmt, 0);
+    result = sqlite3_step(stmt);
+    int count = sqlite3_column_int(stmt, 0);
+    sqlite3_finalize(stmt);
+    return count;
+};
 
 void DBMutantContainerIterator::First()
 {
     sqlite3_stmt *stmt;
-    const char *sql = "SELECT id FROM Mutants ORDER BY id ASC LIMIT 1;";
+    const char *sql = "SELECT ID FROM Mutants ORDER BY ID ASC LIMIT 1;";
     int rc = sqlite3_prepare_v2(DB, sql, -1, &stmt, nullptr);
     rc = sqlite3_step(stmt);
     CurrentId = sqlite3_column_int(stmt, 0);
+    cout << CurrentId << "\n";
     sqlite3_finalize(stmt);
 };
+//------------------- db container functions ----------------------
+void DBMutantContainer::ClearDB()
+{
+    char *errmsg;
+    sqlite3_exec(DB,"DELETE FROM Mutants", NULL, NULL, &errmsg);
+};
+
+void DBMutantContainer::AddMutant(ScumPointer newMutant)
+{
+    string mutanttype = PrintMutantType(newMutant->GetType());
+    string handpower = PrintHandPower(newMutant->GetHandPower());
+    string legpower = PrintLegPower(newMutant->GetLegPower());
+    string ageofmutant = PrintAgeOfMutant(newMutant->GetAgeOfMutant());
+    string query = "INSERT INTO Mutants (MutantType, StrengthOfHands, StrengthOfLegs, Age) VALUES ("
+    +quotesql(mutanttype)+","
+    +quotesql(handpower)+","
+    +quotesql(legpower)+","
+    +quotesql(ageofmutant)+");";
+    char *errmsg;
+    sqlite3_exec(DB, query.c_str(), NULL, NULL, &errmsg);
+    cout << errmsg << "\n";
+};
+
+// ---------------------- factory method----------------------
+Scum *MutantFactory(MutantType newMutant)
+{
+    switch(newMutant)
+    {
+        case MutantType::Gargoyle: return  new Gargoyle;
+		case MutantType::Vampire: return  new Vampire;
+		case MutantType::Wolfman: return  new Wolfman;
+    }
+};
+//----------------- task ---------------------
+
+void task(DBMutantContainerIterator it)
+{
+    it.First();
+    int amount_of_mutants = it.GetCount();
+    for (int i = 0; i<amount_of_mutants; i++)
+    {
+        cout << "~~~~~~~~~~~~~~~~~~~~" << "\n";
+        cout << "Тип: " << it.GetType() << "\n";
+        cout << "Сила ног: " << it.GetHandPower() << "\n";
+        cout << "Сила рук: " << it.GetLegPower() << "\n";
+        cout << "Возраст: " << it.GetAge() << "\n";
+        it.Next();
+    };
+}
 
 int main()
 {
-    DBMutantContainerIterator it("mydb.db");
-    it.First();
-    cout << it.GetType() << "\n";
-}
+    srand(time(NULL));
+    
+    sqlite3* DB;
+    sqlite3_open("mydb.db", &DB);
+    /*
+    DBMutantContainer scumcell(DB);
+    scumcell.ClearDB();
+    int random_amount_of_mutant = random()%(100-10+1)+1;
+    cout << "Генерируем " << random_amount_of_mutant << " мутантов" << "\n";
+    for (int i=0; i<random_amount_of_mutant; i++)
+    {
+        scumcell.AddMutant(MutantFactory(MutantType(rand()%3)));
+    };*/
+    DBMutantContainerIterator IT(DB);
+    task(IT);
+};
--- main.h
+++ main.h
@@ -2,25 +2,79 @@
 
 using namespace std;
 
-class DBMutantContainerIterator
+enum class MutantType : int {Gargoyle, Wolfman, Vampire, Unknown};
+
+enum class StregthOfHands : int {High, Medium, Low};
+
+enum class StregthOfLegs : int {High, Medium, Low};
+
+enum class Age : int {Old, Young, Newborn};
+
+
+class Scum
 {
-private:
-    int CurrentId;
-    sqlite3 * DB;
-    int Count;
-public:
-    DBMutantContainerIterator(const string& DBName)
-    {
-        int db = sqlite3_open(DBName.c_str(), &DB);
-    };
-    void First();
-    int GetCount();
-    string GetHandPower();
-    string GetLegPower();
-    string GetType();
-    string GetAge();
-    void Next() {CurrentId++;};
-    bool IsDone(int i) {return i>Count;};
+    protected:
+        StregthOfHands HandPower;
+        StregthOfLegs LegPower;
+        Age AgeOfMutant;
+        Scum()
+        {
+            HandPower = StregthOfHands(rand()%3);
+            LegPower = StregthOfLegs(rand()%3);
+            AgeOfMutant = Age(rand()%3);
+        };
+    public:
+        StregthOfHands GetHandPower() {return HandPower;};
+        StregthOfLegs GetLegPower() {return LegPower;};
+        Age GetAgeOfMutant() {return AgeOfMutant;};
+        virtual void Summon() const = 0;
+        virtual void Kill() const = 0;
+        virtual MutantType GetType() const = 0;
 };
 
+typedef Scum * ScumPointer;
+
+class DBMutantContainer
+{
+    private:
+        sqlite3* DB;
+    public:
+        DBMutantContainer(sqlite3* db)
+        {
+            DB = db;
+            string createtable = "CREATE TABLE IF NOT EXISTS Mutants ("
+                                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                "MutantType TEXT NOT NULL,"
+                                "StrengthOfHands TEXT,"
+                                "StrengthOfLegs TEXT,"
+                                "Age TEXT"
+                                ");";
+            char *errMsg;
+            sqlite3_exec(DB, createtable.c_str(), nullptr, nullptr, &errMsg);
+            cout << errMsg << "\n";
+        };
+        void AddMutant(ScumPointer newMutant);
+        void ClearDB();
+        void CloseDB() {sqlite3_close(DB);}
+};
+
+class DBMutantContainerIterator
+{
+    private:
+        int CurrentId;
+        sqlite3* DB;
+    public:
+        DBMutantContainerIterator(sqlite3* db)
+        {
+            DB = db;
+        };
+        void First();
+        int GetCount();
+        string GetHandPower();
+        string GetLegPower();
+        string GetType();
+        string GetAge();
+        void Next() {CurrentId++;};
+        //bool IsDone(int i) {return i>Count;};
+};
 #endif FC_sqlH
